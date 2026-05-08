dbb896e68ec654111010d3afdd233be3362b2491
Arjun Sivaraman <arjun140702@gmail.com>
2026-05-07 20:34:11 -0500

    Add functions for qdrant operations

--- utils/vector_store.py
+++ utils/vector_store.py
@@ -1,13 +1,25 @@
+from typing import TypedDict
 from qdrant_client import QdrantClient
 from qdrant_client.http import models
 from langchain_qdrant import QdrantVectorStore
+from langchain_core.documents import Document
 
 from .llm import OpenAIModel
 
 
+class Metadata(TypedDict):
+    filename: str
+    transcript: str
+    visual_description: str
+    title: str
+    description: str
+    thumbnail: str
+
+
 class VectorStore:
-    def __init__(self, qdrant_url: str, collection_name: str, llm: OpenAIModel):
-        q_client = QdrantClient(url=qdrant_url)
+    def __init__(self, url: str, api_key: str, collection_name: str, llm: OpenAIModel):
+        q_client = QdrantClient(url=url, api_key=api_key)
+        self.collection_name = collection_name
 
         collections = q_client.get_collections().collections
         if not any(c.name == collection_name for c in collections):
@@ -17,11 +29,28 @@ class VectorStore:
                     size=1536, distance=models.Distance.COSINE),
             )
 
+        self.client = q_client
         self.store = QdrantVectorStore(
             client=q_client,
-            collection_name=COLLECTION_NAME,
+            collection_name=self.collection_name,
             embedding=llm.get_embedding()
         )
 
     def get_vector_store(self):
         return self.store
+
+    def similarity_search(self, query, retreival_count=5):
+        return self.store.similarity_search(query, k=retreival_count)
+
+    def add_document(self, content: str, metadata: Metadata):
+        self.store.add_documents(
+            [Document(page_content=content, metadata=metadata)])
+
+    def list_documents(self) -> list[Metadata]:
+        records, _ = self.client.scroll(
+            collection_name=self.collection_name,
+            limit=100,  # Adjust limit or implement pagination if library grows large
+            with_payload=True,
+            with_vectors=False  # We don't need to download the giant arrays, just the metadata
+        )
+        return records
